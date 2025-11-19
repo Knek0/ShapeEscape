03df995eb88b81bbd51bf3dba9e34117f61e5761
Knek0 <kiarash.nekonam@outlook.com>
2025-11-19 11:45:32 +0000

    Camera

---
+++ TopDownShooter/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+public class camera : MonoBehaviour
+{
+    //reference to the player object
+    public GameObject player;
+
+    //offset between camera and player
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
